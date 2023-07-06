8910b3d3282d632a184eea4f4f02b1716c76f4d8
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-07-06 15:03:14 -0400

    auth user

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,10 @@ connectDB(); // connecting to mongo database
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
