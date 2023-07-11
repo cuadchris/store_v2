abd8c084096912dab73a7ca4fc352c8be04c5e93
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-07-11 12:28:45 -0400

    initial order page to start testing order slice

---
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,30 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { UseSelector, useSelector } from 'react-redux';
+import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { useEffect } from 'react';
+
+const PlaceOrderScreen = () => {
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    if (!cart.shippingAddress.address) {
+      navigate('/shipping');
+    } else if (!cart.paymentMethod) {
+      navigate('/payment');
+    }
+  }, [cart.shippingAddress.address, cart.paymentMethod, navigate]);
+
+  return (
+    <>
+      <CheckoutSteps step1 step2 step3 step4 />
+      <Row>
+        <Col md={8}>Column 1</Col>
+        <Col md={4}>Column 2</Col>
+      </Row>
+    </>
+  );
+};
+export default PlaceOrderScreen;
