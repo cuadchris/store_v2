2771f6cf6c9a13603e2f1e92fbc8faeaa9ee7c99
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-07-26 13:35:25 -0400

    fleshing out adding/editing a product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,12 @@ import {
   createProduct,
   getProduct,
   getProducts,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
 
-router.route('/:id').get(getProduct);
+router.route('/:id').get(getProduct).put(protect, admin, updateProduct);
 
 export default router;
