164d1b58378f74fa6beaf54b35e9ad62fca632cb
Christopher Cuadrado <cuadradochris@hotmail.com>
2023-08-25 13:35:37 -0400

    search products

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,8 +4,9 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
-import logo from '../assets/logo.png';
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import SearchBox from './SearchBox';
 
 const Header = () => {
   const { cartItems } = useSelector((state) => state.cart);
@@ -39,6 +40,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
+              <SearchBox />
               <LinkContainer to='/cart'>
                 <Nav.Link>
                   <FaShoppingCart /> Cart
